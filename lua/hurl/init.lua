98586546cdd2f0f6911a6b098c943f3dc441f2ec
Jacob Pfeifer <jacob@pfeifer.dev>
2023-01-05 21:30:48 -0500

    register hurl filetype

--- lua/hurl/init.lua
+++ lua/hurl/init.lua
@@ -7,6 +7,14 @@ M.hurl = h.hurl
 M.hurl_no_color = h.hurl_no_color
 
 function M.setup()
+  vim.filetype.add({
+	  extension = {
+	    hurl = "hurl",
+	  },
+	  filename = {
+	    [".hurl"] = "hurl",
+	  }
+	})
   M.register_treesitter()
   vim.api.nvim_create_user_command("Hurl", h.hurl, {})
   vim.api.nvim_create_user_command("HurlNoColor", h.hurl_no_color, {})
