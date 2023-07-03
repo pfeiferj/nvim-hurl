e9dc8da264ee3c4d06cbe30a836c2c8783b41c3a
Jacob Pfeifer <jacob@pfeifer.dev>
2023-07-03 10:09:00 -0400

    add nil check to config

--- lua/hurl/config.lua
+++ lua/hurl/config.lua
@@ -9,6 +9,9 @@ M.config = {
 ---Combines the config with the given user configuration
 ---@param user_config HurlConfig
 function M.update_config(user_config)
+  if user_config == nil then
+    return
+  end
   M.config = vim.tbl_deep_extend("force", M.config, user_config)
 end
 
