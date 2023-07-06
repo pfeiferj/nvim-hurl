c8a55b42ea04b9f965b329989c2b4f572bd8e7b4
Price Hiller <price@orion-technologies.io>
2023-07-06 13:07:24 -0500

    Revert "refactor: use vim.system instead of vim.fn.jobstart"
    This reverts commit 84f763d83710fd7b2247d5ec56ba333fff69f9f7.
    
    This was reverted because it came to my attention (thanks clason) that
    all the `vim` functions that were in use are only valid in Neovim `0.10`
    currently 😔

--- lua/hurl/hurl.lua
+++ lua/hurl/hurl.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+---Checks if a value exists in a given table
+---@param list table
+---@param value any
+---@return boolean
+local function list_contains(list, value)
+  for _, lv in ipairs(list) do
+    if lv == value then
+      return ture
+    end
+  end
+
+  return false
+end
 ---@param config HurlConfig
 function M.hurl(config)
   local gheight = vim.api.nvim_list_uis()[1].height
@@ -17,6 +30,7 @@ function M.hurl(config)
     style = "minimal",
     border = "rounded",
   })
+  local term = vim.api.nvim_open_term(buf,{})
 
   -- This ensures the window created is closed instead of covering the editor when a split is created
   vim.api.nvim_create_autocmd("WinLeave", {
@@ -34,35 +48,24 @@ function M.hurl(config)
   -- Build arguments list
   local hurl_args_t = vim.tbl_deep_extend("force", {}, config.hurl_flags)
   -- Always ensure the `--include` flag is passed so we can get http headers
-  if not vim.list_contains(hurl_args_t, "--include") then
+  if not list_contains(hurl_args_t, "--include") then
     table.insert(hurl_args_t, "--include")
   end
   -- If we're missing color flags then set it according to `config.color`
-  if not vim.list_contains(hurl_args_t, { "--color" }) or not vim.list_contains(hurl_args_t, { "--no-color" }) then
+  if not list_contains(hurl_args_t, { "--color" }) or not list_contains(hurl_args_t, { "--no-color" }) then
     if config.color then
       table.insert(hurl_args_t, "--color")
     else
       table.insert(hurl_args_t, "--no-color")
     end
   end
+  local hurl_args = table.concat(hurl_args_t, " ")
 
-  -- Append the hurl file to target
-  table.insert(hurl_args_t, file)
-  -- Insert the hurl command as the first element of the table. This avoids bugs where the hurl command is in the
-  -- wrong position within the table causing `vim.system` to fail
-  table.insert(hurl_args_t, 1, "hurl")
-  vim.system(
-    hurl_args_t,
-    { text = true },
-    ---@param cmd SystemCompleted
-    function(cmd)
-      vim.schedule(function()
-        local term = vim.api.nvim_open_term(buf, {})
-        vim.api.nvim_chan_send(term, table.concat(vim.split(cmd.stdout, "\n"), "\r\n"))
-        vim.api.nvim_chan_send(term, table.concat(vim.split(cmd.stderr, "\n"), "\r\n"))
-      end)
-    end
-  )
+  vim.fn.jobstart("hurl " .. file .. " " .. hurl_args, {
+    width = width,
+    on_stdout = function(chan, data) vim.api.nvim_chan_send(term,table.concat(data, "\r\n")) end,
+    on_stderr = function(chan, data) vim.api.nvim_chan_send(term,table.concat(data, "\r\n")) end
+  })
 end
 
 return M
